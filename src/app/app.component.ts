07bf537534001f3a1d32300d37c1e41acaa03567
viktoriiabublyk <booblikvm@gmail.com>
2020-11-23 18:58:20 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'TODOapp';
+export class AppComponent implements OnInit {
+  form: FormGroup;
+  posts: Array<object> = [];
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      title: new FormControl('', [Validators.minLength(3), Validators.required]),
+      text: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    });
+
+    this.posts.push({
+      title: 'Title 1',
+      text: 'TExt 1'
+    });
+    this.posts.push({
+      title: 'Title 2',
+      text: 'TExt 2'
+    });
+  }
+
+  submit() {
+    const formData = {...this.form.value};
+
+    let item = {
+      title: formData.title,
+      text: formData.text
+    };
+
+    this.posts.push(item);
+
+    this.form.reset();
+  }
+
+  delete(index) {
+    this.posts.splice(index, 1);
+  }
 }
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
